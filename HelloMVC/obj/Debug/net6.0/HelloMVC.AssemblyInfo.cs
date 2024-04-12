b6c7424c13e6a0216f0e37580c5564c73b197821
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-12 21:58:10 +0700

    add: CSS

--- HelloMVC/obj/Debug/net6.0/HelloMVC.AssemblyInfo.cs
+++ HelloMVC/obj/Debug/net6.0/HelloMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+481512486eed85830369a9d92af0b9c49e71da99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9b6032f6d433ef2a756858bc2a28062f4b09886")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
