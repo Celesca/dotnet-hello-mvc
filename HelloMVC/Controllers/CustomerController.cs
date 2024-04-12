063a25de4d834add8dabc0e58f32d2a99a42044f
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-12 20:22:14 +0700

    add: model and view

---
+++ HelloMVC/Controllers/CustomerController.cs
@@ -0,0 +1,17 @@
+﻿using HelloMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            var CustomerLists = new List<Customer>();
+            CustomerLists.Add(new Customer { CustomerID = 1, FullName = "John Doe", Address = "123 Main St" });
+            CustomerLists.Add(new Customer { CustomerID = 2, FullName = "Jane Doe", Address = "456 Main St" });
+            CustomerLists.Add(new Customer { CustomerID = 3, FullName = "John Smith", Address = "789 Main St" });
+            return View(CustomerLists);
+        }
+    }
+}
