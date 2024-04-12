c8aef8ab46ea379981c9984c16ca8dc83b85be17
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-12 19:24:19 +0700

    add: info view

--- HelloMVC/Controllers/HomeController.cs
+++ HelloMVC/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace HelloMVC.Controllers
             return View();
         }
 
+        public IActionResult Info()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
